ba3cfd5d2ddefb26a66673512919627847dc1f99
satyam raj <satyam3279raj@gmail.com>
2022-08-22 22:59:35 +0530

    added 1.py and 2.py

---
+++ 1.py
@@ -0,0 +1,23 @@
+from __future__ import division
+
+
+print ("hello")
+
+a= 5 or 9 
+print(a)        #prints 5
+b=7 or 6
+print ( b)  #prints 7
+
+
+#if we assign as "a=5 or 9 " or " a= 7 or 6", then the first value will be alloted to the variable. (you know the reason lol)
+
+# parenthesis
+# exponential
+# multiplication
+# division
+# addition
+# subtraction
+
+c=eval(input())     #input like : 4+5       then this statement will set c=4+5==9.      #"eval" keyword is used to allot mathematical expressions in a variable.
+
+print("a", "b", 1, 2, sep="*")      #seperates the words with "*" instead of " ".
